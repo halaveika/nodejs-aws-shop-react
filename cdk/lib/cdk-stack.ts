96511eb3386cb33fa37e4a48f8cf5798b3d41b73
Aliaksandr <halaveika1988@gmail.com>
2023-05-31 01:34:50 +0200

    fix: fix cdk deploy files, package.json scripts

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,38 +1,56 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    const bucketName = 'halaveika-nodejs-aws-shop-react';
-
-    const bucket = new s3.Bucket(this, 'MyAppBucket', {
-      websiteIndexDocument: 'index.html',
-      publicReadAccess: true,
-      bucketName
-    });
-
-    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'MyAppDistribution', {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: bucket,
-          },
-          behaviors: [{ isDefaultBehavior: true }],
-        },
-      ],
-    });
-
-    new BucketDeployment(this, 'DeployStaticAssets', {
-      sources: [Source.asset('../../../dist/')], 
-      destinationBucket: bucket,
-      distribution,
-    });
+export interface CdkStackProps extends cdk.StackProps {
+  bucketName: string;
+  env: {
+    region: string
   }
 }
 
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: CdkStackProps) {
+    super(scope, id, props);
 
+    const { bucketName } = props;
+
+    const bucket = new s3.Bucket(this, 'MyAppBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      websiteIndexDocument: "index.html",
+      bucketName
+    });
+
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAccessIdentity');
+    bucket.grantRead(originAccessIdentity);
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'MyAppDistribution', {
+          originConfigs: [
+            {
+              s3OriginSource: {
+                s3BucketSource: bucket,
+                originAccessIdentity,
+              },
+              behaviors: [{ isDefaultBehavior: true }],
+            },
+          ],
+        }
+    );
+
+    new BucketDeployment(this, 'DeployStaticAssets', {
+      sources: [Source.asset('../dist')],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    new cdk.CfnOutput(this, 'bucketUrl', {
+      value: bucket.bucketWebsiteUrl,
+      exportName: 'bucketUrl',
+    });
+  }
+}
